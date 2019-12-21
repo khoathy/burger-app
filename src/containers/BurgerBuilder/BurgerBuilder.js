9562a8f22911384ca9e271f00bceb36618accb9c
KhoaThy <ms.khoathy@gmail.com>
2019-12-20 18:44:34 -0800

    feat: create the addIngredientHandler, connect state to build controls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -14,11 +14,19 @@ class BurgerBuilder extends Component {
         }
     }
 
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedIngredients = {...this.state.ingredients};
+        updatedIngredients[type] = updatedCount;
+        this.setState({ingredients: updatedIngredients});
+    }
+
     render () {
         return (
             <Aux>
                 <Burger ingredients = {this.state.ingredients}/>
-                <BuildControls />
+                <BuildControls ingredientAdded={this.addIngredientHandler} />
             </Aux>
         )
     }
