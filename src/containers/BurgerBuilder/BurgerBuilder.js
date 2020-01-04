2839b5c2befd6358b471f6cee3abc0b4ac194001
KhoaThy <ms.khoathy@gmail.com>
2020-01-04 07:11:26 -0800

    feat: add orderSummary

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,7 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
     tomato: 0.5,
@@ -82,7 +83,9 @@ class BurgerBuilder extends Component {
         }
         return (
             <Aux>
-                <Modal />
+                <Modal>
+                    <OrderSummary ingredients = {this.state.ingredients}/>
+                </Modal>
                 <Burger ingredients = {this.state.ingredients}/>
                 <BuildControls 
                     ingredientAdded={this.addIngredientHandler}
