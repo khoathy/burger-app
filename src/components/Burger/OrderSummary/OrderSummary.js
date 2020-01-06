19ddff8592837128c3bebb31c723ae89d84fef54
KhoaThy <ms.khoathy@gmail.com>
2020-01-06 00:26:58 -0800

    style : order summary styling

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
+import classes from './OrderSummary.css';
 
 const orderSummary = (props) => {
     const ingredientSummary = Object.keys(props.ingredients)
@@ -13,14 +14,17 @@ const orderSummary = (props) => {
 
     return (
         <Aux>
-            <h3>Your Order</h3>
-            <p> A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p>Continue to checkout?</p>
-            <Button btnType = "Danger" clicked = {props.orderCancel}>Cancel</Button>
-            <Button btnType = "Success" clicked = {props.orderContinue}>Continue</Button>
+            <div className = {classes.OrderSummary}>
+                <h3>Your Order</h3>
+                <p> A delicious burger with the following ingredients:</p>
+                <ul className = {classes.OrderList}>
+                    {ingredientSummary}
+                </ul>
+                <p>Continue to checkout?</p>
+                <Button btnType = "Danger" clicked = {props.orderCancel}>Cancel</Button>
+                <Button btnType = "Success" clicked = {props.orderContinue}>Continue</Button>
+            </div>
+            
         </Aux>
     );
 };
