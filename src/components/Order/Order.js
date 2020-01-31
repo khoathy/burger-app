f26ec37903438ce93697d0b41d79023066a7aad1
KhoaThy <ms.khoathy@gmail.com>
2020-01-30 17:12:09 -0800

    feat: output the orders to orders page

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import classes from './Order.css';
 
-const order = (props) => (
-    <div className={classes.Order}>
-        <p>Ingredients: Salad(1)</p>
-        <p>Price: <strong>USD 5.5</strong></p>
-    </div>
-)
+const order = (props) => {
+
+    //convert to array of ingredients
+    const ingredients =[];
+    for(let property in props.ingredients) {
+        ingredients.push(
+            {
+                ingredientName: property,
+                amount: props.ingredients[property]
+            }
+        );
+    }
+
+    //output ingredients to text
+    const ingredientOutput = ingredients.map(ig => {
+        return  <span className={classes.Ingredient} key={ig.ingredientName}>
+                    {ig.ingredientName} ({ig.amount})
+                </span>
+    })
+
+
+    return (
+        <div className={classes.Order}>
+            <p>Ingredients: {ingredientOutput}</p>
+            <p>Price: <strong>USD {props.price.toFixed(2)}</strong></p>
+        </div>
+    );
+}
 
 
 export default order;
